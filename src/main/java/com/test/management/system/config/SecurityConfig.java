101f9a44040f34c6e7821d0ca98fb22a34e128c1
norika <veronica.shavrova@gmail.com>
2019-12-20 15:27:30 +0200

    fixing icons, fixing swagger. closes#29

--- src/main/java/com/test/management/system/config/SecurityConfig.java
+++ src/main/java/com/test/management/system/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -31,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
+
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
@@ -60,4 +62,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authenticationProvider());
     }
+
+//    @Override
+////    public void configure(WebSecurity webSecurity){
+////        webSecurity.ignoring().antMatchers("/v2/api-docs",
+////                "/configuration/ui",
+////                "/swagger-resources/**",
+////                "/configuration/security",
+////                "/swagger-ui.html",
+////                "/webjars/**");
+////    }
 }
--- src/main/java/com/test/management/system/config/WebConfig.java
+++ src/main/java/com/test/management/system/config/WebConfig.java
@@ -8,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 class WebConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-                .addResourceHandler("/webjars/**", "/resources/**")
-                .addResourceLocations("/webjars/", "/resources/");
+                .addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
+    //"/webjars/**",
 }
